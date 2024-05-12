de3b9f48561d3bffa4dbd3b05740e5279e8ad987
Ivan Isaev <mister.isaevivan@yandex.ru>
2024-05-12 18:05:12 +0300

    Add algorithm tests

---
+++ STLUnitTests/src/test_algorithm.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+
+#include <algorithm>
+
+TEST(test_clamp, test0)
+{
+	// given
+	const auto l = 3.5;
+	const auto h = 4.0;
+
+	// when
+	auto cmp = 3.1;
+	// then
+	ASSERT_DOUBLE_EQ(l, std::clamp(cmp, l, h));
+
+	// when
+	cmp = 3.6;
+	// then
+	ASSERT_DOUBLE_EQ(cmp, std::clamp(cmp, l, h));
+
+	// when
+	cmp = 4.6;
+	// then
+	ASSERT_DOUBLE_EQ(h, std::clamp(cmp, l, h));
+}
