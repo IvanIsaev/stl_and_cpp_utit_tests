a501a36d713965db491e9a26eacbd9fed9fb6f19
isaev_ia <isaev_ia@rengabim.com>
2024-06-10 12:38:40 +0300

    Add get_if variant test

---
+++ STLUnitTests/src/test_variant.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+#include <variant>
+
+namespace TestVariant
+{
+	TEST(test_get_if, test0)
+	{
+		// given
+		auto expectedIntValue = 42;
+		auto intOrDouble = std::variant<int, double>{ expectedIntValue };
+
+		// when
+		auto actualIntValue = std::get_if<int>(&intOrDouble);
+		auto actualDoubleValue = std::get_if<double>(&intOrDouble);
+
+		using namespace testing;
+
+		// then
+		ASSERT_THAT(*actualIntValue, Eq(expectedIntValue));
+		ASSERT_THAT(actualDoubleValue, IsNull());
+	}
+}
