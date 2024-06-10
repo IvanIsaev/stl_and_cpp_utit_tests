ba1928f06a5a00a8b5e3f51baccb1b87f5c93d66
isaev_ia <isaev_ia@rengabim.com>
2024-06-10 19:13:03 +0300

    Add inheritance tests

--- STLUnitTests/src/test_inheritance.cpp
+++ STLUnitTests/src/test_inheritance.cpp
@@ -2,4 +2,70 @@
 
 namespace TestInheritance {
 
+	TEST(test_diamond_inheritance, test0)
+	{
+		class A
+		{
+		public:
+			void foo()
+			{
+
+			}
+		};
+
+		class B : virtual public A
+		{
+
+		};
+
+		class C : virtual public A
+		{
+
+		};
+
+		class D : public B, public C
+		{
+			void doo()
+			{
+				foo();
+			}
+		};
+	}	
+
+	TEST(test_inheritance, test0)
+	{
+		// При наследовании с разными типами (public, protected, private) класс потомок получает доступ к полям базового класса следующим образом
+		// public:
+		// public -> public
+		// protected -> protected
+		// private -> private
+		// 
+		// protected:
+		// public -> protected
+		// protected -> protected
+		// private -> private
+		// 
+		// private:
+		// public -> private
+		// protected -> private
+		// private -> private
+	}
+
+	TEST(test_Public_Morozov, test0)
+	{
+		class Papa
+		{
+		protected:
+			int getMoney() { return 1000000; }
+		};
+
+		class Public_Morozov : public Papa
+		{
+		public:
+			using Papa::getMoney; // Теперь метод getMoney() стал публичным... вот так
+		};
+
+		Public_Morozov pb;
+		ASSERT_THAT(pb.getMoney(), testing::Eq(1000000));
+	}
 }
